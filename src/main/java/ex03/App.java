6b50d8cfdc37f899a5d92bb37b473f6bb19bc843
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 06:35:30 -0400

    Commit 2

--- src/main/java/ex03/App.java
+++ src/main/java/ex03/App.java
@@ -1,4 +1,24 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Zachary Flanagan
+ */
+
 package ex03;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = in.nextLine();
+
+        Scanner in2 = new Scanner(System.in);
+        System.out.print("Who said it? ");
+        String whoSaid = in2.nextLine();
+
+        String outputString = whoSaid + " says, \"" + quote + "\"";
+        System.out.println(outputString);
+    }
 }
